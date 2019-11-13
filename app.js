a50717834f0da12d63f8357ddffc00a34d4ebad0
Altemush Bhatti <Altemush.X.Bhatti@kp.org>
2019-11-13 14:27:05 -0800

    Added keypress event listener to add bedget button

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
+   /* var x = 23;
 
     var add = function(a) {
         return x + a;
@@ -10,7 +11,8 @@ var budgetController = (function() {
         publicTest: function(b) {
             return add(b);
         }//return an object
-    }//publicTest is a closure, it has access
+    }*/
+    //publicTest is a closure, it has access
 //to x and add() even when the IIFE has 
 //already returned
 
@@ -27,7 +29,7 @@ var budgetController = (function() {
 
 //budgetController.publicTest(5);//28
 
-
+// UI CONTROLLER
 var UIController = (function() {
 
 
@@ -36,17 +38,42 @@ var UIController = (function() {
 
 //these 2 controllers don't know about each other
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    /*var z = budgetCtrl.publicTest(5);
 
     return {
         anotherPublic: function() {
             console.log(z);
         }
+    }*/
+
+    var ctrlAddItem = function() {
+        // 1. Get the filed input data
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+
+        console.log('ok');
     }
+
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event)
+    {
+        if(event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
 
 })(budgetController, UIController);
 
-controller.anotherPublic();//28
+//controller.anotherPublic();//28
