1c1d3d6f84594ba4e9a9a1b0de64d15a1d6d5220
Altemush Bhatti <Altemush.X.Bhatti@kp.org>
2019-11-15 11:04:27 -0800

    Added functions to calculate total income, total expense, total budget, and percentage of spent income

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(current){
+            sum += current.value;//current refers to the array element
+        });
+        data.totals[type] = sum;
+    };
+
+    //global data structure
     var data = {//this DS is private, so add
         allItems: {//method to return statement
             exp: [],//so that it can be accessed pubicly
@@ -22,7 +31,9 @@ var budgetController = (function() {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     //public
@@ -56,6 +67,36 @@ var budgetController = (function() {
 
             //return the new element
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            // calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // calculate the budget income - expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent
+            if(data.totals.inc > 0)
+            {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100); 
+            }
+            else 
+            {
+                data.percentage = -1;
+            }
+            
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         },
 
         testing: function() {
@@ -159,10 +200,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
         
         // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         // 3. Display the budget on the UI
+        console.log(budget);
 
     };
 
