5ccaa83eb3bf79df10831260e1481636e4781e62
Altemush Bhatti <Altemush.X.Bhatti@kp.org>
2019-11-18 10:07:16 -0800

    Added functionality to delete item from the UI

--- app.js
+++ app.js
@@ -193,6 +193,12 @@ var UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearfields: function() {
             var fields, fieldsArr;
 
@@ -312,10 +318,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete item from UI
-
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show the new budget
-
+            updateBudget();
 
         }
 
